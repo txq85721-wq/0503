24762793b2ab95d7403b1641f44644c2f4ed0d5d
txq85721-wq <txq85721@gmail.com>
2026-05-03 19:26:04 +0800

    Connect profile page to MySQL API

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,9 +1,28 @@
+const { login, getOpenid } = require('../../utils/auth')
+const app = getApp()
+
 Page({
   data: {
     height: '',
     weight: '',
     goal: '',
-    preference: ''
+    preference: '',
+    budget: '',
+    shoppingFrequency: '',
+    mode: '个人',
+    familySize: '',
+    familyMembers: '',
+    restrictions: '',
+    saving: false
+  },
+
+  async ensureOpenid() {
+    let openid = getOpenid()
+    if (!openid) {
+      const res = await login()
+      openid = res.openid
+    }
+    return openid
   },
 
   onInput(e) {
@@ -11,9 +30,44 @@ Page({
     this.setData({ [field]: e.detail.value })
   },
 
-  saveProfile() {
-    wx.setStorageSync('profile', this.data)
-    wx.showToast({ title: '已保存' })
+  async saveProfile() {
+    try {
+      this.setData({ saving: true })
+      const openid = await this.ensureOpenid()
+      const profile = {
+        height: this.data.height,
+        weight: this.data.weight,
+        goal: this.data.goal,
+        preference: this.data.preference,
+        budget: this.data.budget,
+        shoppingFrequency: this.data.shoppingFrequency,
+        mode: this.data.mode,
+        familySize: this.data.familySize,
+        familyMembers: this.data.familyMembers,
+        restrictions: this.data.restrictions
+      }
+
+      wx.setStorageSync('profile', profile)
+
+      wx.request({
+        url: `${app.globalData.apiBaseUrl}/profile/save`,
+        method: 'POST',
+        data: { openid, profile },
+        header: { 'content-type': 'application/json' },
+        success: (res) => {
+          if (res.statusCode >= 200 && res.statusCode < 300) {
+            wx.showToast({ title: '已保存' })
+          } else {
+            wx.showToast({ title: '保存失败', icon: 'none' })
+          }
+        },
+        fail: () => wx.showToast({ title: '保存失败', icon: 'none' }),
+        complete: () => this.setData({ saving: false })
+      })
+    } catch (e) {
+      this.setData({ saving: false })
+      wx.showToast({ title: '保存失败', icon: 'none' })
+    }
   },
 
   onLoad() {
