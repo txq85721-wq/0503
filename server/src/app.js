1ac62a5f2c952c4cf1fa7e3b5aa6aceb69945881
txq85721-wq <txq85721@gmail.com>
2026-05-03 14:36:36 +0800

    Add server entry

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const recommendRoute = require('./routes/recommend')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('LESSugar API running')
+})
+
+app.use('/api/recommend', recommendRoute)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
